de9f4ea4d9c4b5e6d4ec40b6dc54b2274c04920c
Mjames951 <aspc.james2@gmail.com>
2025-01-22 20:12:26 -0600

    items on store page

--- storefront/models.py
+++ storefront/models.py
@@ -1,2 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.TextField(null=True)
+    posted_at = models.DateTimeField(default=timezone.now)
+    sold = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
+    
--- storefront/views.py
+++ storefront/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item
 
-def store(request):
+def store(request, message=None):
+    items = Item.objects.all()
     return render(request, "storefront/store.html", {
-
+        "items": items,
+        "message": message,
     })
 
-def checkout(request):
+def checkout(request, itemid):
+    try: get_object_or_404(Item, id=itemid)
+    except: return redirect('store', message="Sorry, item doesn't exist or has been sold.")
     return render(request, "storefront/checkout.html", {
         
     })
